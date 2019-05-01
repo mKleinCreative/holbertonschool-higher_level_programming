0b0674667b1153794257bf83c5249f879103db31
mKleinCreative <miketheklein@gmail.com>
2019-05-01 15:47:12 +0000

    pushing up 0 and 1 for testing

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ld = int(repr(number)[-1])
+if (ld < 6 and not 0):
+    print('Last digit of {} is {} and is less than\
+6 and is not 0'.format(number, ld))
+elif (ld > 5):
+    print('Last digit of {} is {} and is greater than 5'.format(number, ld))
+else:
+    print('Last digit of {} is {} and is 0'.format(number, ld))
