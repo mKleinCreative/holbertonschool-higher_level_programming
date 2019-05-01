9783a0a54505a0fc6700ef916cf9f0702bd53dbd
mKleinCreative <miketheklein@gmail.com>
2019-05-01 17:11:57 +0000

    pushing 6 for testing

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+for value in range(1, 80):
+    if (int(str(value).zfill(2)[1]) - int(str(value).zfill(2)[0])) > 0:
+        print('{}'.format(str(value)).zfill(2), end=', ')
+print('89')
